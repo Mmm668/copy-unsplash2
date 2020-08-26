38acb464e372a88c410a554f0437d55732a0d334
BY <devblossom77@gmail.com>
2020-08-26 12:42:50 +0900

    [structure] 이미지, mock.js 추가

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,9 +1,13 @@
 import mainWaterMark from './mainWaterMark.png'
 import userIcon from './user-icon.jpg'
+import mainCoverLogo1 from './main-cover-logo1.png'
+import mainCover from './main-cover.jpeg'
 
 const Images = {
-    mainWaterMark,
     userIcon,
+    mainWaterMark,
+    mainCoverLogo1,
+    mainCover,
 };
 
 export default Images;
---
+++ src/mock/mock.js
@@ -0,0 +1,127 @@
+import {navigate} from "../helpers/HistoryHelper";
+
+export const headerCategory = [
+    {
+        title: 'wallpapers',
+        id: '1065976',
+    },
+    {
+        title: 'business & work',
+        id: '3348877',
+    },
+    {
+        title: 'nature',
+        id: '3330448'
+    },
+    {
+        title: 'people',
+        id: '3356568'
+    },
+    {
+        title: 'unsplash editorial',
+        id: '317099'
+    },
+    {
+        title: 'health',
+        id: '3356594'
+    },
+    {
+        title: 'textures & patterns',
+        id: '3330445'
+    },
+    // repeat
+    {
+        title: 'wallpapers',
+        id: '1065976',
+    },
+    {
+        title: 'business & work',
+        id: '3348877',
+    },
+    {
+        title: 'nature',
+        id: '3330448'
+    },
+    {
+        title: 'people',
+        id: '3356568'
+    },
+    {
+        title: 'unsplash editorial',
+        id: '317099'
+    },
+    {
+        title: 'health',
+        id: '3356594'
+    },
+    {
+        title: 'textures & patterns',
+        id: '3330445'
+    }, {
+        title: 'wallpapers',
+        id: '1065976',
+    },
+    {
+        title: 'business & work',
+        id: '3348877',
+    },
+    {
+        title: 'nature',
+        id: '3330448'
+    },
+    {
+        title: 'people',
+        id: '3356568'
+    },
+    {
+        title: 'unsplash editorial',
+        id: '317099'
+    },
+    {
+        title: 'health',
+        id: '3356594'
+    },
+    {
+        title: 'textures & patterns',
+        id: '3330445'
+    },
+];
+
+export const headerBubbleDropdown = [
+    {
+        text: 'About',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'Wallpapers',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'Blog',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'Collections',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'Community',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'History',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'Made with Unsplash',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'API/Developers',
+        onClick: () => navigate('/about')
+    },
+    {
+        text: 'Official Apps',
+        onClick: () => navigate('/about')
+    },
+]
