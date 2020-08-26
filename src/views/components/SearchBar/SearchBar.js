4fcc699d15645ef498dc6888f2a874098b9b159e
BY <devblossom77@gmail.com>
2020-08-26 12:45:00 +0900

    [cmp] SearchBar 추가

--- src/views/components/SearchBar/SearchBar.js
+++ src/views/components/SearchBar/SearchBar.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled, {css} from "styled-components";
+import {BsSearch} from "react-icons/all";
 
 const SearchBar = (props) => {
 
     const {
-        rounded = false
+        type, // default || 'main'
+        placeholder = 'Search free high-resolution photos',
+        onSubmit,
+        style,
     } = props;
 
+
+    const [keyword, setKeyword] = useState('');
+
     return (
-        <Wrapper rounded={rounded}>
-           SearchBar
+        <Wrapper type={type} style={style}>
+            <Icon onClick={onSubmit}>
+                <BsSearch size={19} color={'#999'}/>
+            </Icon>
+            <input type="text"
+                   placeholder={placeholder}
+                   onKeyPress={(e) => {
+                       console.log('@@ e', e);
+                       // if(key === 'enter') {
+                       //     onSubmit(keyword)
+                       // }
+
+                   }}/>
         </Wrapper>
     )
 };
 
 const Wrapper = styled.div`
-  
-  ${props => props.rounded && css`
-
+  display: flex;
+  flex: 1;
+  align-items: center;
+  height: 38px;
+  border-radius: 25px;
+  background: #eee;
+  input {
+    width: 100%;
+    height: 100%;
+    font-size: 14px;
+  }
+  ${props => props.type === 'main' && css`
+     height: 54px;
+     border-radius: 3px;
+     background:#fff;
+     input{
+       font-size: 17px;
+       &::-webkit-input-placeholder {
+          font-size: 16px;
+       }
+     }
   `}
+`;
+
+const Icon = styled.div`
+  padding: 3px 15px 0;
 `;
 
 export default SearchBar;
