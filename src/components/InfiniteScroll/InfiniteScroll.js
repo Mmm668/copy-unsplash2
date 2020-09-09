32ec609e2ef1a4e7ec77bcd551b9e963e64dfdfe
BY <devblossom77@gmail.com>
2020-09-09 21:18:56 +0900

    무한스크롤 reflow 감소 시도

--- src/components/InfiniteScroll/InfiniteScroll.js
+++ src/components/InfiniteScroll/InfiniteScroll.js
@@ -1,19 +1,25 @@
 import React, {useEffect} from 'react';
 import styled from "styled-components";
 import {useSelector} from "react-redux";
+import Loading from "../Loading/Loading";
 
 const InfiniteScroll = ({children, fetchMore=()=>{}, hasMore, loader, footer}) => {
 
     function infiniteScrollListener() {
+        let tick;
         let docHeight = document.body.clientHeight;
         let scrollBottom = document.documentElement.scrollTop + window.innerHeight; // scrollTop is lack of full content height.
         let percentScroll = Math.floor(scrollBottom / docHeight * 100);
 
-        if (window.oldScroll < window.scrollY) {
-            if(percentScroll === 85){ // 85이상 시 1번만 발생하도록 => 일단 85때만 실행
+
+        if(tick === undefined || window.oldScroll < window.scrollY){
+            if(percentScroll > 85){
                 console.log('@@ scroll 85%----------');
                 fetchMore();
+                tick = true;
             }
+        } else if (tick && percentScroll < 85){
+            tick = false;
         }
         window.oldScroll = window.scrollY;
     }
@@ -31,6 +37,7 @@ const InfiniteScroll = ({children, fetchMore=()=>{}, hasMore, loader, footer}) =
             {loader}
             {children}
             {footer}
+            <Loading style={{position: 'absolute', left: 0, right: 0, bottom: 0}}/>
         </Wrapper>
 
     )
