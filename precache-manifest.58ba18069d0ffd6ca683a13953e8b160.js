a5a2cf604f88b57b59c4a3c2f454ac4ee26bb471
BY <devblossom77@gmail.com>
2020-09-09 20:24:14 +0900

    Updates

---
+++ precache-manifest.58ba18069d0ffd6ca683a13953e8b160.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3dbad3d80736b58df0250261e642a84",
+    "url": "/copy-unsplash2/index.html"
+  },
+  {
+    "revision": "3169866e1955c26bec25",
+    "url": "/copy-unsplash2/static/css/main.6ce3c5e4.chunk.css"
+  },
+  {
+    "revision": "fe448359d67c2d123038",
+    "url": "/copy-unsplash2/static/js/2.e1b00b79.chunk.js"
+  },
+  {
+    "revision": "c607541aec0fb4887a90f60037cb0bdf",
+    "url": "/copy-unsplash2/static/js/2.e1b00b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3169866e1955c26bec25",
+    "url": "/copy-unsplash2/static/js/main.3e6000a1.chunk.js"
+  },
+  {
+    "revision": "212533af6dbe833cf1ed",
+    "url": "/copy-unsplash2/static/js/runtime-main.53a0099a.js"
+  },
+  {
+    "revision": "a9e9856b2b2f67285ce3bf0f9fc47360",
+    "url": "/copy-unsplash2/static/media/main-cover-logo1.a9e9856b.png"
+  },
+  {
+    "revision": "f86ef8e8b491c9366e9a27ca678168f8",
+    "url": "/copy-unsplash2/static/media/main-cover.f86ef8e8.jpeg"
+  },
+  {
+    "revision": "b434d4a0b70d7e2c71de9014733b37aa",
+    "url": "/copy-unsplash2/static/media/mainWaterMark.b434d4a0.png"
+  }
+]);
